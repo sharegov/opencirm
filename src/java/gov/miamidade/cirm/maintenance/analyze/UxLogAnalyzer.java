bb6d6a3922b7f7359eb459358cc26f495c529c0c
hilpold <thomas.hilpold@gmail.com>
2016-10-24 08:37:35 -0400

    2986 Log file analyzer should handle rotating logs automatically

--- src/java/gov/miamidade/cirm/maintenance/analyze/UxLogAnalyzer.java
+++ src/java/gov/miamidade/cirm/maintenance/analyze/UxLogAnalyzer.java
@@ -21,6 +21,7 @@ import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 /**
  * A simple log file User Experience analyzer that prints some hourly statistics and daily percentiles.<br>
  * <br>
@@ -30,8 +31,9 @@ import java.util.regex.Pattern;
  * <br>
  * Usage:<br>
  * 1. Modify logMatch<br>
- * 2. Modify logPaths<br>
- * 3. Modify BAD_UX_THRESHOLD_SECS<br>
+ * 2. Modify ANALYSIS_DAYS_HISTORY_CUTOFF_DEFAULT<br>
+ * 3. Modify rotatingLogSeriesBaseFiles<br>
+ * 4. Modify BAD_UX_THRESHOLD_SECS<br>
  * Run as java program.<br>
  * <br>
  * @author Thomas Hilpold
@@ -39,80 +41,20 @@ import java.util.regex.Pattern;
  */
 class UxLogAnalyzer {
 	
+	static final int ANALYSIS_DAYS_HISTORY_CUTOFF_DEFAULT = 14;	
+	
 	static final boolean DBG_MATCHES = false;
 	
 	static final double BAD_UX_THRESHOLD_SECS = 10;	
 	
 	static String logMatch = "END createNewKOSR"; //"END updateServiceCase data"; //"END createNewKOSR";
 			
-	static String[] logPaths = new String[] {
+	static String[] rotatingLogSeriesBaseFiles = new String[] {
 			"\\\\s2030050\\cirmservices\\logs\\wrapper.log",
-			"\\\\s2030050\\cirmservices\\logs\\wrapper.log.1",
-			"\\\\s2030050\\cirmservices\\logs\\wrapper.log.2",
-			"\\\\s2030050\\cirmservices\\logs\\wrapper.log.3",
-			"\\\\s2030050\\cirmservices\\logs\\wrapper.log.4",
-			"\\\\s2030050\\cirmservices\\logs\\wrapper.log.5",
-			"\\\\s2030050\\cirmservices\\logs\\wrapper.log.6",
 			"\\\\s2030051\\cirmservices\\logs\\wrapper.log",
-			"\\\\s2030051\\cirmservices\\logs\\wrapper.log.1",
-			"\\\\s2030051\\cirmservices\\logs\\wrapper.log.2",
-			"\\\\s2030051\\cirmservices\\logs\\wrapper.log.3",
-			"\\\\s2030051\\cirmservices\\logs\\wrapper.log.4",
-			"\\\\s2030051\\cirmservices\\logs\\wrapper.log.5",
-			"\\\\s2030051\\cirmservices\\logs\\wrapper.log.6",
 			"\\\\s2030057\\cirmservices\\logs\\wrapper.log",
-			"\\\\s2030057\\cirmservices\\logs\\wrapper.log.1",
-			"\\\\s2030057\\cirmservices\\logs\\wrapper.log.2",
-			"\\\\s2030057\\cirmservices\\logs\\wrapper.log.3",
-			"\\\\s2030057\\cirmservices\\logs\\wrapper.log.4",
-			"\\\\s2030057\\cirmservices\\logs\\wrapper.log.5",
-			"\\\\s2030057\\cirmservices\\logs\\wrapper.log.6",
 			"\\\\s2030059\\cirmservices\\logs\\wrapper.log",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.1",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.2",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.3",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.4",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.5",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.6",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.7",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.8",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.9",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.10",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.11",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.12",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.13",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.14",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.15",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.16",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.17",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.18",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.19",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.20",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.21",
-			"\\\\s2030059\\cirmservices\\logs\\wrapper.log.22",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.1",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.2",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.3",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.4",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.5",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.6",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.7",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.8",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.9",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.10",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.11",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.12",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.13",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.14",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.15",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.16",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.17",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.18",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.19",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.20",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.21",
-			"\\\\s2030060\\cirmservices\\logs\\wrapper.log.22"
+			"\\\\s2030060\\cirmservices\\logs\\wrapper.log"
 	};
 	
 	private final static DateFormat logDf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
@@ -125,25 +67,107 @@ class UxLogAnalyzer {
 	
 	void run() {
 		System.out.println("***** UX Log analyzer started *****");
-		System.out.println("Files to analyze: " + logPaths.length);
+		System.out.println("Rotating base log files to analyze: " + rotatingLogSeriesBaseFiles.length);
 		System.out.println("Line Match: " + logMatch);
-		for (String logFile : logPaths) {
+		Date cutOffDate = determineCutoffDay(ANALYSIS_DAYS_HISTORY_CUTOFF_DEFAULT);
+		System.out.println("Start day for analysis: " + cutOffDate);		
+		for (String logFile : rotatingLogSeriesBaseFiles) {
 			File cur = new File(logFile);
-			if (cur.isFile() && cur.canRead()) {
-				System.out.print("Analyzing " + cur + "...");
-				analyzeFile(cur, logMatch);
-				System.out.println("Complete.");
-			}
+			analyzeRotatingLogFileSeries(cur, logMatch, cutOffDate);
 		}
 		stats.printHourly();
 		stats.printDayPercentiles();
 	}
 	
-	void analyzeFile(File file, String lineMatch) {
-		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+	/**
+	 * Determines the oldes day in history for which analysis should be conducted.
+	 * e..g if analysisHistoryTargetDays = 1, yesterday 00:00:00 will be returned
+	 * @param analysisHistoryTargetDays
+	 * @return
+	 */
+	Date determineCutoffDay(int analysisHistoryTargetDays) {
+		Calendar cutoff = Calendar.getInstance();
+		cutoff.set(Calendar.HOUR_OF_DAY, 0);
+		cutoff.set(Calendar.MINUTE, 0);
+		cutoff.set(Calendar.SECOND, 0);
+		cutoff.set(Calendar.MILLISECOND, 0);
+		cutoff.add(Calendar.DAY_OF_MONTH, -analysisHistoryTargetDays);
+		return cutoff.getTime();
+	}
+	
+	/**
+	 * Starts analysis with the base log file and finds other log files in series and goes back in time until cutOffMinDate.
+	 * Log files in series with a last modified date prior to cutoff are ignored.
+	 * 
+	 * @param baseLogFile
+	 * @param lineMatch
+	 * @param cutOffMinDate
+	 */
+	void analyzeRotatingLogFileSeries(File baseLogFile, String lineMatch, Date cutOffMinDate) {
+		int rotatingLogNumber = 0;
+		boolean continueNextRotatingLog;
+		do {
+			continueNextRotatingLog = false;
+			File curLogFile = getRotatingLogFile(baseLogFile, rotatingLogNumber);
+			if (curLogFile != null) {
+				Date curLogFileLastModified = new Date(curLogFile.lastModified());
+				continueNextRotatingLog = (curLogFileLastModified.after(cutOffMinDate) || curLogFile.equals(cutOffMinDate));
+				if (continueNextRotatingLog) {
+					System.out.println("Analyzing " + curLogFile+ " (LastMod: " + logDf.format(curLogFileLastModified) + ")");
+					analyzeOneRotatingLogFile(curLogFile, lineMatch, cutOffMinDate);
+				} else {
+					System.out.println("Log history was complete.");
+				}
+			} else {
+				System.err.println("Log history incomplete for this series.");
+				System.err.println("Rotating log " + rotatingLogNumber + " either not found or inaccessible. Increase log.");
+			}
+			rotatingLogNumber ++;
+		} while (continueNextRotatingLog);		
+	}
+	
+	/**
+	 * Returns the log file by adding .1,.2,.3 to determine other files in log series.
+	 * Also checks if the log file exists and can be read.
+	 * 
+	 * @param baseLogFile
+	 * @param rotatingSuffixNumber
+	 * @return
+	 */
+	File getRotatingLogFile(File baseLogFile, int rotatingSuffixNumber) {
+		if (rotatingSuffixNumber == 0) {
+			return baseLogFile;
+		} else {
+			String suffix = "." + rotatingSuffixNumber;
+			String fullPath = baseLogFile.getAbsolutePath() + suffix;
+			File f = new File(fullPath);
+			if (f.exists() && f.canRead()) { 
+				return f;
+			} else {
+				return null;
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param curLogfile
+	 * @param lineMatch
+	 * @param cutOffMinDate
+	 * @return true if all matched lines in file have dates after cutOffminDate 
+	 */
+	boolean analyzeOneRotatingLogFile(File curLogFile, String lineMatch, Date cutOffMinDate) {
+		boolean continueWithNextFile = true;
+		try (BufferedReader br = new BufferedReader(new FileReader(curLogFile))) {
 		    String line;
 		    while ((line = br.readLine()) != null) {
-		       analyzeLine(line, lineMatch);
+		    	Date curLineDate = analyzeLine(line, lineMatch, cutOffMinDate);
+		    	if (curLineDate != null && (curLineDate.before(cutOffMinDate) && continueWithNextFile)) {
+		    		//this file has a match with a date prior to cutOff
+		    		//we'll still read the  whole file, but return false to indicate that the next (older)
+		    		//rotating log file does not need to be processed
+		    		continueWithNextFile = false;
+		    	}
 		    }
 		    br.close();
 		} catch (FileNotFoundException e) {
@@ -151,6 +175,7 @@ class UxLogAnalyzer {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return continueWithNextFile;
 	}
 	/**
 	 * Analyzes a line by matching and then finding dateTime and duration for that sample.
@@ -158,23 +183,27 @@ class UxLogAnalyzer {
 	 * 
 	 * @param line
 	 * @param lineMatch a string expected between the last | and the duration second double value.
+	 * @return lineDateTime or null if not determined
 	 */
-	void analyzeLine(String line, String lineMatch) {
+	Date analyzeLine(String line, String lineMatch, Date cutoff) {
 		Pattern p = Pattern.compile("(^.+\\|.+\\|([\\d\\/\\s:]+)\\|.+" + lineMatch + "([\\s\\d.]+)sec.*)");
 		Matcher m = p.matcher(line);
+		Date lineDateTime = null;
 		if (m.find()) {
 			String dateTimeStr = m.group(2).trim();
 			String durationStr = m.group(3).trim();
-			Date dateTime;
 			try {
-				dateTime = logDf.parse(dateTimeStr);
+				lineDateTime = logDf.parse(dateTimeStr);
 				Double durationSecs = Double.parseDouble(durationStr);
-				stats.addSample(dateTime, lineMatch, durationSecs);
-				if (DBG_MATCHES) System.out.println(line + " >> Found: " + durationSecs + " DT: " + dateTimeStr);
+				if (lineDateTime.after(cutoff)) {
+					stats.addSample(lineDateTime, lineMatch, durationSecs);
+					if (DBG_MATCHES) System.out.println(line + " >> Found: " + durationSecs + " DT: " + dateTimeStr);
+				}
 			} catch (ParseException e) {
 				System.out.println("Ignoring line: " + line);
 			}
 		}
+		return lineDateTime;
 	}
 	
 	/**
@@ -282,7 +311,7 @@ class UxLogAnalyzer {
 		long nrOfSamples;
 		double minDuration = Double.MAX_VALUE;
 		double maxDuration;
-		double nrOfBadUx;
+		int nrOfBadUx;
 		
 		/**
 		 * Adds one sample duration sec value to this stats entry object. 
