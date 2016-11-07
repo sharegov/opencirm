828f11a39289d9efbf316dd5f9b02c685dbb275b
syed <sabbas@miamidade.gov>
2016-11-07 16:13:06 -0500

    Added maintenance scripts and aws test configuration.

--- bin/scripts/cirmadmin-dev-restart-freshdb.cmd
+++ bin/scripts/cirmadmin-dev-restart-freshdb.cmd
@@ -1,3 +1,5 @@
 sc \\s0144654 stop cirmservices
+ping -n 5 127.0.0.1>null
 del \\s0144654\c$\cirmservices\dbConf\* /Q
 sc \\s0144654 start cirmservices
+ping -n 5 127.0.0.1>null
--- bin/scripts/repo-server-refresh-prod2test.cmd
+++ bin/scripts/repo-server-refresh-prod2test.cmd
@@ -1,9 +1,13 @@
 @echo off
 sc \\s0020269 stop owldbTest
+ping -n 5 127.0.0.1>null
 mkdir \\s0020269\c$\owldbTest\data-backup_%date:~-4,4%%date:~-10,2%%date:~-7,2%
 copy \\s0020269\c$\owldbTest\data \\s0020269\c$\owldbTest\data-backup_%date:~-4,4%%date:~-10,2%%date:~-7,2%
 del \\s0020269\c$\owldbTest\data\* /Q
 sc \\s0144737 stop ontologyServer
+ping -n 5 127.0.0.1>null
 copy \\s0144737\c$\ontologyServer\data \\s0020269\c$\owldbTest\data
 sc \\s0144737 start ontologyServer
+ping -n 5 127.0.0.1>null
 sc \\s0020269 start owldbTest
+ping -n 5 127.0.0.1>null
