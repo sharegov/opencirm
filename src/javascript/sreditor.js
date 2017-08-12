3a9bd9ca3aa1d05b8b6925b7bd3ccc63e24ef73b
hilpold <thomas.hilpold@gmail.com>
2017-08-11 22:48:10 -0400

    3511 COM: Upload file option not working Added filename detection for cases where full path is sent to server. Try catch to metadata upload added, effective on new SR with image.

--- src/javascript/sreditor.js
+++ src/javascript/sreditor.js
@@ -2154,13 +2154,19 @@ define(["jquery", "U", "rest", "uiEngine", "store!", "cirm", "legacy", "interfac
                     self.removeDuplicates();
                     setModel(result.bo, model, model.srType(), type, true, false);
                
-                    var meta = []; 
-                    meta.push(new metadata("sr",srid));
-                    meta.push(new metadata("type",type));
+                    
                    
                     //2149 Optional Image upload - disabled meta until next week
                     //Enabled again
-                    updateMetadata(meta, self.data().properties().hasAttachment()); 
+                    try {
+                    	var meta = []; 
+                        meta.push(new metadata("sr",srid));
+                        meta.push(new metadata("type",type));
+                        
+                    	updateMetadata(meta, self.data().properties().hasAttachment());
+                    } catch(err) {
+                    	console.log("Error during meta: " + err);
+                    }
                 }
                 else if(result.ok == false) {
                     $("#sh_save_progress").dialog('close');
